14622db84ff95418a4dbe85622507cb1c7ba81ea
Fernando Santos <fern@ndo.io>
2021-02-01 00:43:06 -0300

    WIP Importing NPM Packages

--- packages/common/src/apple-pay.ts
+++ packages/common/src/apple-pay.ts
@@ -1,4 +1,4 @@
-import { DonationAmount, EnForm } from "@4site/engrid-events/src";
+import { DonationAmount } from "@4site/engrid-events";
 
 /*global window */
 const ApplePaySession = (window as any).ApplePaySession;
