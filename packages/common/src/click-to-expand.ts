6fb86fcf5029de8fa7dfb1e9f7eb80ede20f00e8
bryancasler <bryan.casler@gmail.com>
2021-03-08 13:56:13 -0500

    Trying to get click-to-expand to work with keyboard users focus the element and click Enter or Space to expand it

--- packages/common/src/click-to-expand.ts
+++ packages/common/src/click-to-expand.ts
@@ -1,14 +1,21 @@
 // Depends on engrid-click-to-expand.scss to work
 // Works when the user has adds ".click-to-expand" as a class to any field
 export class ClickToExpand {
-    clickToExpandWrapper = document.querySelectorAll('div.click-to-expand');
+    clickToExpandWrapper = document.querySelectorAll('div.click-to-expand') as NodeListOf<HTMLElement>;
+    
     constructor() {
         if (this.clickToExpandWrapper.length) {
-            this.clickToExpandWrapper.forEach((element) => {
+            this.clickToExpandWrapper.forEach((element: HTMLElement) => {
                 const content = element.innerHTML;
-                const wrapper_html = '<div class="click-to-expand-cta"></div><div class="click-to-expand-text-wrapper">' + content + '</div>';
+                const wrapper_html = '<div class="click-to-expand-cta"></div><div class="click-to-expand-text-wrapper" tabindex="0">' + content + '</div>';
                 element.innerHTML = wrapper_html;
                 element.addEventListener("click", this.expand.bind(this, element as HTMLElement));
+                element.addEventListener("keydown", event => {
+                    if ((event as KeyboardEvent).key === 'Enter' || (event as KeyboardEvent).key === 'Space') {
+                        console.log("Enter or Space Bar was clicked while focusing on a click-to-expand div");
+                        this.expand.bind(element as HTMLElement);
+                    }
+                  });
             });
         }
     }
@@ -16,4 +23,5 @@ export class ClickToExpand {
     private expand(element: HTMLElement) {
         element.classList.add("expanded");
     }
+
 }
