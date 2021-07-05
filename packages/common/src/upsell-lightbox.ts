49ae67c62afe1f5ddc4a37302642195fe95ce26d
Fernando Santos <fern@ndo.io>
2021-07-05 20:13:04 -0300

    Fix: Get upsell lightbox amount BEFORE changing frequency

--- packages/common/src/upsell-lightbox.ts
+++ packages/common/src/upsell-lightbox.ts
@@ -199,8 +199,8 @@ export class UpsellLightbox {
       // In the circumstance when the form fails to validate via server-side validation, the page will reload
       // When that happens, we should place the original amount saved in sessionStorage into the upsell original amount field
       let original = window.sessionStorage.getItem('original');
-      if(original && document.querySelectorAll('.en__errorList .en__error').length > 0) {
-          this.setOriginalAmount(original);
+      if (original && document.querySelectorAll('.en__errorList .en__error').length > 0) {
+        this.setOriginalAmount(original);
       }
 
       // Returning true will give the "go ahead" to submit the form
@@ -215,25 +215,25 @@ export class UpsellLightbox {
 
   // Set the original amount into a hidden field using the upsellOriginalGiftAmountFieldName, if provided
   private setOriginalAmount(original: string) {
-      if (this.options.upsellOriginalGiftAmountFieldName) {
-          let enFieldUpsellOriginalAmount = document.querySelector(".en__field__input.en__field__input--hidden[name='" + this.options.upsellOriginalGiftAmountFieldName + "']");
-          if (!enFieldUpsellOriginalAmount) {
-              let pageform = document.querySelector("form.en__component--page");
-              if (pageform) {
-                let input = document.createElement("input");
-                input.setAttribute("type", "hidden");
-                input.setAttribute("name", this.options.upsellOriginalGiftAmountFieldName);
-                input.classList.add('en__field__input', 'en__field__input--hidden');
-                pageform.appendChild(input);
-                enFieldUpsellOriginalAmount = document.querySelector('.en__field__input.en__field__input--hidden[name="' + this.options.upsellOriginalGiftAmountFieldName + '"]');
-              }
-          }
-          if (enFieldUpsellOriginalAmount) {
-              // save it to a session variable just in case this page reloaded due to server-side validation error
-              window.sessionStorage.setItem('original', original);
-              enFieldUpsellOriginalAmount.setAttribute("value", original);
-          }
+    if (this.options.upsellOriginalGiftAmountFieldName) {
+      let enFieldUpsellOriginalAmount = document.querySelector(".en__field__input.en__field__input--hidden[name='" + this.options.upsellOriginalGiftAmountFieldName + "']");
+      if (!enFieldUpsellOriginalAmount) {
+        let pageform = document.querySelector("form.en__component--page");
+        if (pageform) {
+          let input = document.createElement("input");
+          input.setAttribute("type", "hidden");
+          input.setAttribute("name", this.options.upsellOriginalGiftAmountFieldName);
+          input.classList.add('en__field__input', 'en__field__input--hidden');
+          pageform.appendChild(input);
+          enFieldUpsellOriginalAmount = document.querySelector('.en__field__input.en__field__input--hidden[name="' + this.options.upsellOriginalGiftAmountFieldName + '"]');
+        }
       }
+      if (enFieldUpsellOriginalAmount) {
+        // save it to a session variable just in case this page reloaded due to server-side validation error
+        window.sessionStorage.setItem('original', original);
+        enFieldUpsellOriginalAmount.setAttribute("value", original);
+      }
+    }
   }
 
   // Proceed to the next page (upsold or not)
@@ -242,8 +242,9 @@ export class UpsellLightbox {
     if (e.target instanceof Element && document.querySelector("#upsellYesButton")?.contains(e.target)) {
       if (ENGrid.debug) console.log("Upsold");
       this.setOriginalAmount(this._amount.amount.toString());
+      const upsoldAmount = this.getUpsellAmount();
       this._frequency.setFrequency("monthly");
-      this._amount.setAmount(this.getUpsellAmount());
+      this._amount.setAmount(upsoldAmount);
     } else {
       this.setOriginalAmount('');
       window.sessionStorage.removeItem('original');
