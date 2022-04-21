048dd80a05402c5aca532d4e971e0324d018a985
Fernando Santos <fern@ndo.io>
2022-04-21 15:39:55 -0300

    Version-bump

--- packages/common/dist/version.d.ts
+++ packages/common/dist/version.d.ts
@@ -1,1 +1,1 @@
-export declare const AppVersion = "0.11.10";
+export declare const AppVersion = "0.11.11";
--- packages/common/dist/version.js
+++ packages/common/dist/version.js
@@ -1,1 +1,1 @@
-export const AppVersion = "0.11.10";
+export const AppVersion = "0.11.11";
