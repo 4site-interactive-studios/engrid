c74af3c6f86cb80196d17a4a979d0e036cf49d6c
Fernando Santos <fern@ndo.io>
2022-05-19 14:56:16 -0300

    v0.11.16

--- packages/common/dist/version.d.ts
+++ packages/common/dist/version.d.ts
@@ -1,1 +1,1 @@
-export declare const AppVersion = "0.11.15";
+export declare const AppVersion = "0.11.16";
--- packages/common/src/version.ts
+++ packages/common/src/version.ts
@@ -1,1 +1,1 @@
-export const AppVersion = "0.11.15";
+export const AppVersion = "0.11.16";
