d12df03b799b8972b79ca0fcf15122e380e025e7
bryancasler <bryan.casler@gmail.com>
2022-06-30 23:34:27 -0400

    v0.12.18

--- packages/common/dist/version.d.ts
+++ packages/common/dist/version.d.ts
@@ -1,1 +1,1 @@
-export declare const AppVersion = "0.12.17";
+export declare const AppVersion = "0.12.18";
--- packages/common/dist/version.js
+++ packages/common/dist/version.js
@@ -1,1 +1,1 @@
-export const AppVersion = "0.12.17";
+export const AppVersion = "0.12.18";
