8f8167070d06e3ff190e1b4127107abcc1523a9d
Fernando Santos <fern@ndo.io>
2022-07-22 17:23:32 -0300

    Ecard Component Updates

--- packages/common/dist/ecard.js
+++ packages/common/dist/ecard.js
@@ -1,10 +1,49 @@
-import { ENGrid, EnForm } from "./";
+import { ENGrid, EnForm, EngridLogger } from "./";
 export class Ecard {
     constructor() {
         this._form = EnForm.getInstance();
-        if (ENGrid.getPageType() === "ECARD") {
-            this._form.onValidate.subscribe(() => this.checkRecipientFields());
+        this.logger = new EngridLogger("Ecard", "red", "#f5f5f5", "🪪");
+        if (!this.shouldRun())
+            return;
+        this._form.onValidate.subscribe(() => this.checkRecipientFields());
+        const schedule = ENGrid.getUrlParameter("engrid_ecard.schedule");
+        const scheduleField = ENGrid.getField("ecard.schedule");
+        const name = ENGrid.getUrlParameter("engrid_ecard.name");
+        const nameField = document.querySelector(".en__ecardrecipients__name input");
+        const email = ENGrid.getUrlParameter("engrid_ecard.email");
+        const emailField = document.querySelector(".en__ecardrecipients__email input");
+        if (schedule && scheduleField) {
+            // Check if chedule date is in the past
+            const scheduleDate = new Date(schedule.toString());
+            const today = new Date();
+            if (scheduleDate.setHours(0, 0, 0, 0) < today.setHours(0, 0, 0, 0)) {
+                // If it is, set the schedule to today
+                scheduleField.value = ENGrid.formatDate(today, "YYYY-MM-DD");
+            }
+            else {
+                // Otherwise, set the schedule to the date provided
+                scheduleField.value = schedule.toString();
+            }
+            this.logger.log("Schedule set to " + scheduleField.value);
         }
+        if (name && nameField) {
+            nameField.value = name.toString();
+            this.logger.log("Name set to " + nameField.value);
+        }
+        if (email && emailField) {
+            emailField.value = email.toString();
+            this.logger.log("Email set to " + emailField.value);
+        }
+        // Replace the Future Delivery Label with a H2
+        const futureDeliveryLabel = document.querySelector(".en__ecardrecipients__futureDelivery label");
+        if (futureDeliveryLabel) {
+            const futureDeliveryH2 = document.createElement("h2");
+            futureDeliveryH2.innerText = futureDeliveryLabel.innerText;
+            futureDeliveryLabel.replaceWith(futureDeliveryH2);
+        }
+    }
+    shouldRun() {
+        return ENGrid.getPageType() === "ECARD";
     }
     checkRecipientFields() {
         const addRecipientButton = document.querySelector(".en__ecarditems__addrecipient");
--- packages/common/src/ecard.ts
+++ packages/common/src/ecard.ts
@@ -1,12 +1,62 @@
-import { ENGrid, EnForm } from "./";
+import { ENGrid, EnForm, EngridLogger } from "./";
 
 export class Ecard {
   public _form: EnForm = EnForm.getInstance();
+  private logger: EngridLogger = new EngridLogger(
+    "Ecard",
+    "red",
+    "#f5f5f5",
+    "🪪"
+  );
 
   constructor() {
-    if (ENGrid.getPageType() === "ECARD") {
-      this._form.onValidate.subscribe(() => this.checkRecipientFields());
+    if (!this.shouldRun()) return;
+    this._form.onValidate.subscribe(() => this.checkRecipientFields());
+    const schedule = ENGrid.getUrlParameter("engrid_ecard.schedule");
+    const scheduleField = ENGrid.getField("ecard.schedule") as HTMLInputElement;
+    const name = ENGrid.getUrlParameter("engrid_ecard.name");
+    const nameField = document.querySelector(
+      ".en__ecardrecipients__name input"
+    ) as HTMLInputElement;
+    const email = ENGrid.getUrlParameter("engrid_ecard.email");
+    const emailField = document.querySelector(
+      ".en__ecardrecipients__email input"
+    ) as HTMLInputElement;
+
+    if (schedule && scheduleField) {
+      // Check if chedule date is in the past
+      const scheduleDate = new Date(schedule.toString());
+      const today = new Date();
+      if (scheduleDate.setHours(0, 0, 0, 0) < today.setHours(0, 0, 0, 0)) {
+        // If it is, set the schedule to today
+        scheduleField.value = ENGrid.formatDate(today, "YYYY-MM-DD");
+      } else {
+        // Otherwise, set the schedule to the date provided
+        scheduleField.value = schedule.toString();
+      }
+      this.logger.log("Schedule set to " + scheduleField.value);
     }
+    if (name && nameField) {
+      nameField.value = name.toString();
+      this.logger.log("Name set to " + nameField.value);
+    }
+    if (email && emailField) {
+      emailField.value = email.toString();
+      this.logger.log("Email set to " + emailField.value);
+    }
+    // Replace the Future Delivery Label with a H2
+    const futureDeliveryLabel = document.querySelector(
+      ".en__ecardrecipients__futureDelivery label"
+    ) as HTMLLabelElement;
+    if (futureDeliveryLabel) {
+      const futureDeliveryH2 = document.createElement("h2");
+      futureDeliveryH2.innerText = futureDeliveryLabel.innerText;
+      futureDeliveryLabel.replaceWith(futureDeliveryH2);
+    }
+  }
+
+  private shouldRun() {
+    return ENGrid.getPageType() === "ECARD";
   }
 
   private checkRecipientFields() {
