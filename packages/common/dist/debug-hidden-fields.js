3b7592c649e03cdd2766a8a3d86e84c2f2ae2e66
bryancasler <bryan.casler@gmail.com>
2023-04-02 16:57:49 -0400

    Transform hidden input fields to text and wrap with custom markup
    This update modifies hidden input fields by changing their type to 'text' and wrapping them in custom markup, including a label and container divs. The resulting structure provides better styling and functionality while preserving any attached event listeners.

--- packages/common/dist/debug-hidden-fields.js
+++ packages/common/dist/debug-hidden-fields.js
@@ -3,19 +3,39 @@ import { EngridLogger } from ".";
 export class DebugHiddenFields {
     constructor() {
         this.logger = new EngridLogger("Debug hidden fields", "#f0f0f0", "#ff0000", "🫣");
+        // Query all hidden input elements within the specified selectors
         const fields = document.querySelectorAll(".en__component--row [type='hidden'], .engrid-added-input[type='hidden']");
+        // Check if there are any hidden fields
         if (fields.length > 0) {
+            // Log the names of the hidden fields being changed to type 'text'
             this.logger.log(`Switching the following type 'hidden' fields to type 'text':  ${[
                 ...fields,
             ]
                 .map((f) => f.name)
                 .join(", ")}`);
+            // Iterate through each hidden input element
             fields.forEach((el) => {
+                // Change the input type to 'text' and add the required classes
                 el.type = "text";
-                el.setAttribute("unhidden", "");
+                el.classList.add("en__field__input", "en__field__input--text");
+                // Create a new label element and set its text and classes
                 const label = document.createElement("label");
-                label.textContent = "Hidden field:" + el.name;
-                el.insertAdjacentElement("beforebegin", label);
+                label.textContent = "Hidden field: " + el.name;
+                label.classList.add("en__field__label");
+                // Create a new 'div' element for the input field and add the required classes
+                const fieldElement = document.createElement("div");
+                fieldElement.classList.add("en__field__element", "en__field__element--text");
+                // Create a new 'div' container for the label and input field, and add the required classes and attribute
+                const fieldContainer = document.createElement("div");
+                fieldContainer.classList.add("en__field", "en__field--text", "hide");
+                fieldContainer.setAttribute("unhidden", "");
+                fieldContainer.appendChild(label);
+                fieldContainer.appendChild(fieldElement);
+                // Insert the new field container before the original input element and move the input element into the field element div
+                if (el.parentNode) {
+                    el.parentNode.insertBefore(fieldContainer, el);
+                    fieldElement.appendChild(el);
+                }
             });
         }
     }
