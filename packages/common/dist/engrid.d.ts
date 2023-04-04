da1213893c455a5f2eeb093b6639eecd2cddf942
Fernando Santos <fern@ndo.io>
2023-04-04 15:01:49 -0300

    New TWEETPAGE Page Type

--- packages/common/dist/engrid.d.ts
+++ packages/common/dist/engrid.d.ts
@@ -16,7 +16,7 @@ export declare abstract class ENGrid {
     static getPageID(): any;
     static getClientID(): any;
     static getDataCenter(): "us" | "ca";
-    static getPageType(): "DONATION" | "ECARD" | "SURVEY" | "EMAILTOTARGET" | "ADVOCACY" | "SUBSCRIBEFORM" | "SUPPORTERHUB" | "UNSUBSCRIBE" | "UNKNOWN";
+    static getPageType(): "DONATION" | "ECARD" | "SURVEY" | "EMAILTOTARGET" | "ADVOCACY" | "SUBSCRIBEFORM" | "SUPPORTERHUB" | "UNSUBSCRIBE" | "TWEETPAGE" | "UNKNOWN";
     static setBodyData(dataName: string, value: string | boolean): void;
     static getBodyData(dataName: string): string | null;
     static hasBodyData(dataName: string): boolean;
