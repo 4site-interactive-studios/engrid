9a19d2acd33a3c8c2cd26203e4c0f6c798a3a7fa
Fernando Santos <fern@ndo.io>
2023-06-06 23:54:51 -0300

    v0.13.74

--- packages/common/dist/version.d.ts
+++ packages/common/dist/version.d.ts
@@ -1,1 +1,1 @@
-export declare const AppVersion = "0.13.73";
+export declare const AppVersion = "0.13.74";
--- packages/common/dist/version.js
+++ packages/common/dist/version.js
@@ -1,1 +1,1 @@
-export const AppVersion = "0.13.73";
+export const AppVersion = "0.13.74";
