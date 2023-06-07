2ca3b9a4142e10f8d20fba58b2577d0a6009556a
Fernando Santos <fern@ndo.io>
2023-06-06 23:54:30 -0300

    UniversalOptIn: Add support to universal-opt-in_null

--- packages/common/dist/universal-opt-in.js
+++ packages/common/dist/universal-opt-in.js
@@ -3,12 +3,15 @@
  * inside a universal opt-in element (any form block with the CSS class universal-opt-in). When the user clicks on a radio/checkbox
  * button, we will search for every other radio/checkbox button inside the same
  * universal opt-in element and mirror the user's selection.
+ * If instead of universal-opt-in you use universal-opt-in_null, the class will
+ * not mirror the user's selection when a radio "No" option is selected. Instead,
+ * it will unset all other radio buttons.
  */
-import { EngridLogger } from ".";
+import { EngridLogger, ENGrid } from ".";
 export class UniversalOptIn {
     constructor() {
         this.logger = new EngridLogger("UniversalOptIn", "#f0f0f0", "#d2691e", "🪞");
-        this._elements = document.querySelectorAll(".universal-opt-in");
+        this._elements = document.querySelectorAll(".universal-opt-in, .universal-opt-in_null");
         if (!this.shouldRun())
             return;
         this.addEventListeners();
@@ -58,21 +61,26 @@ export class UniversalOptIn {
                         if (yesNoValue === "Y") {
                             this.logger.log("Yes/No " + yesNoElement.getAttribute("type") + " is checked");
                             yesNoElements.forEach((yesNoElement2) => {
-                                if (yesNoElement === yesNoElement2)
+                                const fieldName = yesNoElement2.getAttribute("name");
+                                const clickedFieldName = yesNoElement.getAttribute("name");
+                                if (!fieldName || fieldName === clickedFieldName)
                                     return;
-                                if (yesNoElement2.getAttribute("value") === "Y")
-                                    yesNoElement2.setAttribute("checked", "checked");
-                                if (yesNoElement2.getAttribute("value") === "N")
-                                    yesNoElement2.removeAttribute("checked");
+                                ENGrid.setFieldValue(fieldName, "Y");
                             });
                         }
                         else {
                             this.logger.log("Yes/No " + yesNoElement.getAttribute("type") + " is unchecked");
                             yesNoElements.forEach((yesNoElement2) => {
-                                if (yesNoElement === yesNoElement2)
+                                const fieldName = yesNoElement2.getAttribute("name");
+                                const clickedFieldName = yesNoElement.getAttribute("name");
+                                if (!fieldName || fieldName === clickedFieldName)
                                     return;
-                                if (yesNoElement2.getAttribute("value") === "Y")
-                                    yesNoElement2.removeAttribute("checked");
+                                if (element.classList.contains("universal-opt-in")) {
+                                    ENGrid.setFieldValue(fieldName, "N");
+                                }
+                                else {
+                                    yesNoElement2.checked = false;
+                                }
                             });
                         }
                     });
--- packages/common/src/universal-opt-in.ts
+++ packages/common/src/universal-opt-in.ts
@@ -3,8 +3,11 @@
  * inside a universal opt-in element (any form block with the CSS class universal-opt-in). When the user clicks on a radio/checkbox
  * button, we will search for every other radio/checkbox button inside the same
  * universal opt-in element and mirror the user's selection.
+ * If instead of universal-opt-in you use universal-opt-in_null, the class will
+ * not mirror the user's selection when a radio "No" option is selected. Instead,
+ * it will unset all other radio buttons.
  */
-import { EngridLogger } from ".";
+import { EngridLogger, ENGrid } from ".";
 
 export class UniversalOptIn {
   private logger: EngridLogger = new EngridLogger(
@@ -13,8 +16,9 @@ export class UniversalOptIn {
     "#d2691e",
     "🪞"
   );
-  private _elements: NodeListOf<HTMLElement> =
-    document.querySelectorAll(".universal-opt-in");
+  private _elements: NodeListOf<HTMLElement> = document.querySelectorAll(
+    ".universal-opt-in, .universal-opt-in_null"
+  );
 
   constructor() {
     if (!this.shouldRun()) return;
@@ -34,7 +38,7 @@ export class UniversalOptIn {
     this._elements.forEach((element) => {
       const yesNoElements = element.querySelectorAll(
         ".en__field__input--radio, .en__field__input--checkbox"
-      );
+      ) as NodeListOf<HTMLInputElement>;
       if (yesNoElements.length > 0) {
         yesNoElements.forEach((yesNoElement) => {
           yesNoElement.addEventListener("click", () => {
@@ -79,20 +83,24 @@ export class UniversalOptIn {
               );
 
               yesNoElements.forEach((yesNoElement2) => {
-                if (yesNoElement === yesNoElement2) return;
-                if (yesNoElement2.getAttribute("value") === "Y")
-                  yesNoElement2.setAttribute("checked", "checked");
-                if (yesNoElement2.getAttribute("value") === "N")
-                  yesNoElement2.removeAttribute("checked");
+                const fieldName = yesNoElement2.getAttribute("name");
+                const clickedFieldName = yesNoElement.getAttribute("name");
+                if (!fieldName || fieldName === clickedFieldName) return;
+                ENGrid.setFieldValue(fieldName, "Y");
               });
             } else {
               this.logger.log(
                 "Yes/No " + yesNoElement.getAttribute("type") + " is unchecked"
               );
               yesNoElements.forEach((yesNoElement2) => {
-                if (yesNoElement === yesNoElement2) return;
-                if (yesNoElement2.getAttribute("value") === "Y")
-                  yesNoElement2.removeAttribute("checked");
+                const fieldName = yesNoElement2.getAttribute("name");
+                const clickedFieldName = yesNoElement.getAttribute("name");
+                if (!fieldName || fieldName === clickedFieldName) return;
+                if (element.classList.contains("universal-opt-in")) {
+                  ENGrid.setFieldValue(fieldName, "N");
+                } else {
+                  yesNoElement2.checked = false;
+                }
               });
             }
           });
