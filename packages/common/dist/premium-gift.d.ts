45ea0776798914f957e81a735ae2afc44d270e1c
Michael Thomas <michaelthomas372@gmail.com>
2024-05-15 17:43:52 +0200

    restore premium selection after amount/frequency change

--- packages/common/dist/premium-gift.d.ts
+++ packages/common/dist/premium-gift.d.ts
@@ -1,9 +1,12 @@
 export declare class PremiumGift {
     private logger;
     private enElements;
+    private _frequency;
+    private _amount;
     constructor();
     shoudRun(): boolean;
     addEventListeners(): void;
+    restorePremiumGift(): void;
     checkPremiumGift(): void;
     searchElements(): void;
     setPremiumTitle(title: string): void;
