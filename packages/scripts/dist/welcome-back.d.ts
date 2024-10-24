0a7152d9644eab136abc2eee60d846d62b725d9f
Michael Thomas <michaelthomas372@gmail.com>
2024-10-24 12:44:14 +0200

    fix welcome back interaction with remember me checkbox

--- packages/scripts/dist/welcome-back.d.ts
+++ packages/scripts/dist/welcome-back.d.ts
@@ -1,7 +1,9 @@
 export declare class WelcomeBack {
     private supporterDetails;
     private options;
+    private rememberMeEvents;
     constructor();
+    private run;
     private shouldRun;
     private addWelcomeBack;
     private resetWelcomeBack;
