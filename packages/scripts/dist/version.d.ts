a1413fbe71a88aa0959bb8f07266c8998dcb0c1a
Fernando Santos <fern@ndo.io>
2024-11-12 13:26:57 -0500

    v0.19.18

--- packages/scripts/dist/version.d.ts
+++ packages/scripts/dist/version.d.ts
@@ -1,1 +1,1 @@
-export declare const AppVersion = "0.19.17";
+export declare const AppVersion = "0.19.18";
--- packages/scripts/src/version.ts
+++ packages/scripts/src/version.ts
@@ -1,1 +1,1 @@
-export const AppVersion = "0.19.17";
+export const AppVersion = "0.19.18";
