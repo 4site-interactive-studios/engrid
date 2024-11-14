f50b55e206bf0227ccdb81a883063bb5d7e49d25
Michael Thomas <michaelthomas372@gmail.com>
2024-11-14 15:01:50 +0100

    fix autofilling and submitting forms with email addresses or chain parameter

--- packages/scripts/src/url-to-form.ts
+++ packages/scripts/src/url-to-form.ts
@@ -20,7 +20,10 @@ export class UrlToForm {
     this.urlParams.forEach((value, key) => {
       const field = document.getElementsByName(key)[0] as HTMLInputElement;
       if (field) {
-        if (!["text", "textarea"].includes(field.type) || !field.value) {
+        if (
+          !["text", "textarea", "email"].includes(field.type) ||
+          !field.value
+        ) {
           ENGrid.setFieldValue(key, value);
           this.logger.log(`Set: ${key} to ${value}`);
         }
