c4a4327cc396e0ca6e1a56435c5c3bb75b4b5580
npgiano-macbook <npgiano@gmail.com>
2025-10-24 18:24:49 -0400

    Use inmem check box to help determine embedded ecard visibility

--- packages/scripts/dist/embedded-ecard.js
+++ packages/scripts/dist/embedded-ecard.js
@@ -94,8 +94,15 @@ export class EmbeddedEcard {
         return iframe;
     }
     addEventListeners() {
+        var _a;
+        const inMemoryCheckbox = document.getElementById("en__field_transaction_inmem");
         const sendEcardCheckbox = document.getElementById("en__field_embedded-ecard");
-        this.toggleEcardForm(sendEcardCheckbox.checked);
+        this.toggleEcardForm(((_a = inMemoryCheckbox === null || inMemoryCheckbox === void 0 ? void 0 : inMemoryCheckbox.checked) !== null && _a !== void 0 ? _a : true) && sendEcardCheckbox.checked);
+        inMemoryCheckbox === null || inMemoryCheckbox === void 0 ? void 0 : inMemoryCheckbox.addEventListener("change", (e) => {
+            const checkbox = e.target;
+            const _sendEcardCheckbox = document.getElementById("en__field_embedded-ecard");
+            this.toggleEcardForm(checkbox.checked && _sendEcardCheckbox.checked);
+        });
         sendEcardCheckbox === null || sendEcardCheckbox === void 0 ? void 0 : sendEcardCheckbox.addEventListener("change", (e) => {
             const checkbox = e.target;
             this.toggleEcardForm(checkbox.checked);
